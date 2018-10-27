79131f16ce57b2f31ab16e0b3bb6c01d0b749a0a
ShainaR <shainaread@gmail.com>
2018-10-27 12:09:47 -0500

    fixed some compiling problems and added bin so you could run the main classes

--- src/controller/TaskDataAccessor.java
+++ src/controller/TaskDataAccessor.java
@@ -1,14 +1,18 @@
 package controller;
 import model.Task;
+import model.ToDoList;
 
 import java.sql.*;
-
+import java.util.Date;
 import java.util.List ;
 import java.util.ArrayList ;
 
 
 public class TaskDataAccessor {
 	private Connection connection;
+	
+	// need to add connection pooling here instead of using Driver Manager - caches
+	// connections to the db to reduce overhead of connecting to the db repeatedly
 	
 	public TaskDataAccessor(String driverClassName, String dbURL, String user, String password) throws SQLException, ClassNotFoundException{
 		Class.forName(driverClassName);
@@ -43,6 +47,33 @@ public class TaskDataAccessor {
 		}
 	}
 	
-	// add other methods like add task, delete task, update task, etc.
+	// add Task
+	public void addTask(String name, Date create, Date due, String sum, String grp, Date remind, int pri, boolean stat) throws SQLException{
+		Task newTask = new Task();
+		
+		Statement stmnt = connection.createStatement();
+		int insertSuccessful = stmnt.executeUpdate("INSERT INTO Task (TaskName, TaskCreateDate, TaskDueDate, Summary, Category, Reminder, Priority, Status) " + 
+				"VALUES ( '" + name + "', '" + create + "', '" + due + "', '" + sum + "', '" + grp + "', '" + remind + "', '" + pri + "', '" + stat + "')");
+		if(insertSuccessful == 1)
+			System.out.println("Insert was successful");
+		else 
+			System.out.println("Insert failed");
+	}
+	
+	// delete Task
+	
+	// filter by TaskName
+	
+	// filter by TaskCreateDate
+	
+	// filter by TaskDueDate
+	
+	// filter by Group
+	
+	// filter by Priority
+	
+	// filter by Status
+	
+	// display filtered
 	
 }
